23cf76c4ecf28c04917beb756ec0119fee5d5c5f
stoffffff <nafi.professional@outlook.com>
2025-07-07 18:39:00 +0000

    last try or/else switch to manual gke cluster then use pipeline to deploy to it

--- terraform/gke.tf
+++ terraform/gke.tf
@@ -1,49 +1,37 @@
+provider "google" {
+  project = var.project_id
+  region  = var.region    
+  zone    = "europe-west1-b"    
+}
+
 resource "google_container_cluster" "primary" {
-  name     = var.gke_cluster_name
-  location = var.region                   
-  network  = google_compute_network.vpc_network.name
-  subnetwork = google_compute_subnetwork.staging_subnet.name
+  name     = "flask-app-cluster"
+  location = "europe-west1-b"  
 
   remove_default_node_pool = true
-
-  initial_node_count = 1
-  
-  workload_identity_config {
-    workload_pool = "${var.project_id}.svc.id.goog"
-  }
-
-  logging_service    = "logging.googleapis.com/kubernetes"
-  monitoring_service = "monitoring.googleapis.com/kubernetes"
+  initial_node_count       = 1
 }
 
 resource "google_container_node_pool" "primary_nodes" {
-  name     = "flask-app-pool"
-  cluster  = google_container_cluster.primary.name
-  location = var.region
+  name       = "default-node-pool"
+  cluster    = google_container_cluster.primary.name
+  location   = google_container_cluster.primary.location
   node_count = 1
 
   node_config {
-    machine_type = "e2-micro"          
+    machine_type = "e2-medium"
+
+    disk_type    = "pd-balanced"
+    disk_size_gb = 30
+
     oauth_scopes = [
       "https://www.googleapis.com/auth/cloud-platform",
     ]
 
-    workload_metadata_config {
-      mode = "GKE_METADATA"
-    }
-
-    tags = ["flask-app-node"]
-  }
-
-  autoscaling {
-    min_node_count = 1
-    max_node_count = 2
   }
 
   management {
-    auto_upgrade = true
     auto_repair  = true
+    auto_upgrade = true
   }
-
-  depends_on = [google_container_cluster.primary]
 }
