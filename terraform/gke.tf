e289789c11c1eaaaa23860bfc2484f1a729aeac1
stoffffff <nafi.professional@outlook.com>
2025-07-07 18:19:30 +0000

    try to fix quota issue

--- terraform/gke.tf
+++ terraform/gke.tf
@@ -4,10 +4,17 @@ resource "google_container_cluster" "primary" {
   network  = google_compute_network.vpc_network.name
   subnetwork = google_compute_subnetwork.staging_subnet.name
 
+  node_config {
+    machine_type = "e2-micro"          
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform",
+    ]
+  }
+
   remove_default_node_pool = true
 
   initial_node_count = 1
-  
+
   workload_identity_config {
     workload_pool = "${var.project_id}.svc.id.goog"
   }
