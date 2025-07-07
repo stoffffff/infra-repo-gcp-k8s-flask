0b892d3a3d8a010c7f68ff270713f9a299f199f9
stoffffff <nafi.professional@outlook.com>
2025-07-07 15:59:58 +0000

    switch to autopilot mode

--- terraform/gke.tf
+++ terraform/gke.tf
@@ -2,28 +2,12 @@ resource "google_container_cluster" "primary" {
   name     = var.gke_cluster_name
   location = var.region
 
+  enable_autopilot = true
+
   network    = google_compute_network.vpc_network.name
   subnetwork = google_compute_subnetwork.staging_subnet.name
 
-  initial_node_count       = 1
-  remove_default_node_pool = true
-
   workload_identity_config {
     workload_pool = "${var.project_id}.svc.id.goog"
-  }
-}
-
-resource "google_container_node_pool" "primary_nodes" {
-  name     = "primary-nodes"
-  cluster  = google_container_cluster.primary.name
-  location = google_container_cluster.primary.location
-
-  node_count = 3
-
-  node_config {
-    machine_type = "e2-micro"        
-    disk_type    = "pd-ssd"          
-    disk_size_gb = 30                
-    image_type   = "COS_CONTAINERD"  
   }
 }
