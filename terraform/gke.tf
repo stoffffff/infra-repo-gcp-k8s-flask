f352809b2c7106e11ed9ec87b238a4bd494ee7dc
stoffffff <nafi.professional@outlook.com>
2025-07-07 11:30:10 +0000

    change gke cluster machine type to n1-standard-1

--- terraform/gke.tf
+++ terraform/gke.tf
@@ -18,7 +18,7 @@ resource "google_container_node_pool" "primary_nodes" {
   node_count = 2
 
   node_config {
-    machine_type = "e2-medium"
+    machine_type = "n1-standard-1"
     oauth_scopes = [
       "https://www.googleapis.com/auth/cloud-platform",
     ]
