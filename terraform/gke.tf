ce6a870ef61ecc05dbda8d5a1d3e41085ad83dd4
stoffffff <nafi.professional@outlook.com>
2025-07-07 18:40:30 +0000

    remove provider duplication

--- terraform/gke.tf
+++ terraform/gke.tf
@@ -1,9 +1,3 @@
-provider "google" {
-  project = var.project_id
-  region  = var.region    
-  zone    = "europe-west1-b"    
-}
-
 resource "google_container_cluster" "primary" {
   name     = "flask-app-cluster"
   location = "europe-west1-b"  
